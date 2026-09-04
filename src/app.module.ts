a7454e5813de6c57bdd4a2a0c19732fdd4f91579
Dmitrij <131361134+Nero-XE@users.noreply.github.com>
2026-09-04 16:46:29 +0500

    Revert "feat: базовый каркас приложения с Prisma и GraphQL"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module.js';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
-import { ProfileModule } from './profile/profile.module.js';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-      graphiql: false,
-      plugins: [ApolloServerPluginLandingPageLocalDefault()]
-    }),
-    PrismaModule,
-    ProfileModule,
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
