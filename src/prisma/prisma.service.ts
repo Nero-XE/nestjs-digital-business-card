6308042d96df335167e1b25a8e0b764fa5d12db8
Nero-XE <dimakoshkarov@outlook.com>
2026-09-04 16:00:36 +0500

    feat: базовый каркас приложения с Prisma и GraphQL

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(private readonly configService: ConfigService) {
+    const connectionString = configService.getOrThrow<string>('DATABASE_URL');
+    const adapter = new PrismaPg({ connectionString });
+    super({ adapter });
+  }
+}
