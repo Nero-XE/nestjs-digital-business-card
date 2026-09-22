8ff8394c9d0e00f71cf71f1b7e494e4c6ebedf45
Nero-XE <dimakoshkarov@outlook.com>
2026-09-22 16:12:15 +0500

    refactor(config): рефакторинг конфигурации приложения и файла окружения

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from './common/interfaces/env.interface.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService<EnvironmentVariables>)
   app.useGlobalPipes(new ValidationPipe())
 
-  const port = process.env.PORT ?? 3000;
-
+  const port = config.getOrThrow<number>('PORT')
   await app.listen(port);
 }
 await bootstrap();
